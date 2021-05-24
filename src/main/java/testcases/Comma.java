23c13a39056a72aeaaad3b80563be0c38532df13
Boopathi <Boopathi>
2021-05-24 15:38:36 +0530

    jhjb

---
+++ src/main/java/testcases/Comma.java
@@ -0,0 +1,12 @@
+package testcases;
+
+public class Comma {
+ public static void main(String []args){
+		   String strMain = "Alpha, Beta, Delta, Gamma, Sigma";
+		    String[] arrSplit = strMain.split(", ");
+		    for (int i=0; i < arrSplit.length; i++)
+		    {
+		      System.out.println(arrSplit[i]);
+		    }
+		  }
+		}
