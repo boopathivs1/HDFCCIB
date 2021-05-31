8e06d01db0ae859ceb02dc8c1e71b10f91f770a0
User <User@DESKTOP-RV3G2MV>
2021-05-31 14:21:48 +0530

    ok

---
+++ src/main/java/testcases/Tc_56_Onscreen_Payment_Single_Without_verify.java
@@ -0,0 +1,99 @@
+package testcases;
+import java.io.IOException;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import com.relevantcodes.extentreports.ExtentTest;
+
+import pages.HDFC_Login_Page;
+import wrappers.ProjectWrapp;
+public class Tc_56_Onscreen_Payment_Single_Without_verify  extends ProjectWrapp {
+	@BeforeClass(groups={"common"})
+	public void setDatag() {
+		testCaseName="TC56";
+		testDescription="Onscreen_Payment_Single_Without_verify";
+		browserName="Chrome";
+		dataSheetName="HDFCCIB";
+		category="Regression";
+		authors="Sreejith";
+		testKeyword="TC56";
+		LogoutStatus=true;	
+		usertype="CIBUser";
+	}
+	@Test(groups={"sanity"},dataProvider="fetch")
+	public void checkAccSummary(String casename,String userid,String pwd,String groupid,
+			String selectformat,String amount,String authuserid,String authpwd,String authgroupid,String compid
+			,String SelectStatus,String Paytype,String compBranch,String accno,String benecode,String data12,String data13,
+			String data14,String data15,String data16,String data17,String data18,String data19,String data20,String data21) throws Exception{
+		
+		
+		try{
+			String accnum=regvalue(accno);
+			getnewurl();
+
+			new	HDFC_Login_Page(driver, test)
+.fillCredentials(userid, pwd, groupid)
+.clickFundTransfers()
+.clickAcctoAccLink()
+.clickAccSingleLink()
+.fillAcctoAccSingleform(selectformat,compid,compBranch,accnum,benecode, amount)
+//.fillAcctoAccSingleform(selectformat, amount)
+.getrefnum()
+.clickLogout();		
+		getnewurl();		
+		new	HDFC_Login_Page(driver, test)
+.fillCredentials(authuserid, authpwd, authgroupid)
+.fundTransfersLink()
+.clickAuthorize()
+.contentFrame()
+.filterAuthorize(compid, SelectStatus, Paytype)
+.clickViewLink()
+.authrecordFrame()
+.clickreflink()
+.clickfirstAuthRec()
+.getAuthVerifyStatus("VP")
+.CheckVerifyStatus()
+.defaultcontents()
+.contentFrame()	
+.clickViewLink()
+.authrecordFrame()
+.clickreflink()
+.clickfirstAuthRec()
+.submitPwd(pwd)
+.contentFrame()
+.clickViewLink()
+.authrecordFrame()
+.clickreflink()
+.getAuthStatus("A")
+.checkStatus();
+		
+//.clickfirstAuthRec()
+//.confirmRecord()
+//.contentFrame()
+//.clickViewLink()
+//.authrecordFrame()
+//.clickreflink()
+//.getAuthStatus("UP")
+//.defaultcontents()
+//.contentFrame()		
+//.clickViewLink()
+//.authrecordFrame()
+//.clickreflink()
+//.getFinalStatus();
+		
+defaultcontent();		
+clickLogoutLink();
+
+		
+		}
+	catch (Exception e) {
+clickLogoutLink();
+		throw new Exception();
+		
+		// TODO: handle exception
+	}
+
+	}
+	}
