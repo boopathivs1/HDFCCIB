9cd04a9d363970e5a26de866b6ba6c67d6428551
User <User@DESKTOP-RV3G2MV>
2021-06-14 16:36:28 +0530

    ok

--- src/main/java/pages/EPI_Payment_SimulatorPage.java
+++ src/main/java/pages/EPI_Payment_SimulatorPage.java
@@ -36,7 +36,7 @@ public class EPI_Payment_SimulatorPage extends ProjectWrapp{
 	public EPI_Payment_SimulatorPage FillEPIPCSSimulator(String MerchantNo){
 		
 		 
-		enterByXpathExplict(prop.getProperty("enter.epipcs.merchantno.xpath"), MerchantNo+gettwoRandomNumber_range_String(20,99));
+		enterByXpathExplict(prop.getProperty("enter.epipcs.merchantno.xpath"), MerchantNo+getRandomNumberString());
 		
 		clickByXpathExplict(prop.getProperty("click.epi.submit.xpath"));
 		
